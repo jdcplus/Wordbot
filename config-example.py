f0ea75250d99faaa327a70f2a0b8d0efa8f772c6
Jigar Chandvania <jdcmerc@gmail.com>
2018-04-22 00:36:03 +0530

    tweaks something

--- config-example.py
+++ config-example.py
@@ -1,15 +1,15 @@
 base_url = ' '
 
-bot_token = 'XXXX-XXXX'
+bot_token = '560289592:AAEMGOKzqN3S-tS3LxhD_F7kgAXQ2Jzku3g'
 
-test_url = 'https://api.telegram.org/<Your Bot Token>/getUpdates'
+test_url = 'https://api.telegram.org/bot560289592:AAEMGOKzqN3S-tS3LxhD_F7kgAXQ2Jzku3g/getUpdates'
 
 glosbe_url = 'https://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase='
 
 thesaurus_api_key = 'XXXX XXXX'
 
-start_message = ''' Enter some message'''
+start_message = ''' Oops ! It seems that I don't know that :( '''
 
 wordnik_url = 'http://api.wordnik.com:80/v4/word.json/'
 
-wordnik_api_key = 'your wordnik api key'
+wordnik_api_key = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
